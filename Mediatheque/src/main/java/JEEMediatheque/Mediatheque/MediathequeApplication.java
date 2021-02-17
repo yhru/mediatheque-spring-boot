57bd5cf659dc499fc04c1f8922ccacf1757c3e79
yhru <yhruvok@gmail.com>
2021-02-17 16:13:53 +0100

    Commit Spring Initializr

---
+++ Mediatheque/src/main/java/JEEMediatheque/Mediatheque/MediathequeApplication.java
@@ -0,0 +1,13 @@
+package JEEMediatheque.Mediatheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediathequeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediathequeApplication.class, args);
+	}
+
+}
---
+++ Mediatheque/src/test/java/JEEMediatheque/Mediatheque/MediathequeApplicationTests.java
@@ -0,0 +1,13 @@
+package JEEMediatheque.Mediatheque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediathequeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
