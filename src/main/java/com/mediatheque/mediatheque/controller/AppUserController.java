61469448b6ea0a3d0dad81d6ddddaa757c8e92e4
yhru <yhruvok@gmail.com>
2021-05-15 13:27:45 +0200

    (feature) - AppUserController

---
+++ src/main/java/com/mediatheque/mediatheque/controller/AppUserController.java
@@ -0,0 +1,24 @@
+package com.mediatheque.mediatheque.controller;
+
+import com.mediatheque.mediatheque.entities.AppUser;
+import com.mediatheque.mediatheque.services.AppUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping(path = "api/v1/users")
+public class AppUserController {
+    private final AppUserService appUserService;
+
+    @Autowired
+    public AppUserController(AppUserService appUserService) {
+        this.appUserService = appUserService;
+    }
+
+    @GetMapping
+    public List<AppUser> getUser(){ return appUserService.getUser(); }
+}
+
